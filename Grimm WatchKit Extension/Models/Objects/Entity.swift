c9eec441891d4b86076a0eb47042285852a9c636
Tyler <ternarytyler@gmail.com>
2017-11-30 18:50:24 -0800

    Added documentation

--- Grimm WatchKit Extension/Models/Objects/Entity.swift
+++ Grimm WatchKit Extension/Models/Objects/Entity.swift
@@ -8,25 +8,52 @@
 
 import Foundation
 
+/// An object that represents a moveable entity
+/// within Grimm capable of manageing items and
+/// and acting on effects.
+///
+/// This object was built mainly for the `player`
+/// property in `PlayerController`, but multiple
+/// entities interacting with each other is
+/// possible with some work.
 class Entity: Codable {
+    /// List of items held an entity.
     var inventory = [Item]()
+    
+    /// Location in which the entity resides.
     var location: WorldLocation
+    
+    /// Area that contains the entity.
     var area: Area
     
+    /// Contructs an `Entity` instance
+    ///
+    /// - Parameters:
+    ///   - location: Location at which to spawn
+    ///   - parent: Area that contains the entity
     init(at location: WorldLocation, parent: Area) {
         self.location = location
         self.area = parent
     }
     
+    /// Sets the inventory held by the entity.
+    ///
+    /// - Note: Meant to be used in method chaining
+    /// - Parameter items: Items to be set (inventory)
+    /// - Returns: self (current instance)
     @discardableResult
     func with(items: [Item]) -> Entity {
         self.inventory = items
         return self
     }
     
+    /// Moves the entity in a direction.
+    ///
+    /// - Important: Does not check if
+    /// - Parameter direction: Direction to move
     func move(in direction: Direction) {
         var destination = self.location
-        destination.area += direction.unitVec
+        destination.areaIndex += direction.unitVec
         move(to: destination)
     }
     
--- Grimm WatchKit Extension/Models/Objects/Item.swift
+++ Grimm WatchKit Extension/Models/Objects/Item.swift
@@ -9,19 +9,61 @@
 import Foundation
 
 class Item: Codable {
+    /// Name of the item.
+    ///
+    /// This property is displayed to the user
+    /// in the item interface.
     var name: String
-    var health: Int
     
+    /// Health of the item.
+    ///
+    /// Ranges from 0 - 100. Health value will
+    /// snap to nearest valid value in not in
+    /// range.
+    var health: Int {
+        didSet(oldHealth) {
+            if health < 0 {
+                health = 0
+            } else if health > 100 {
+                health = 100
+            }
+        }
+    }
+    
+    /// Contructs an `Item` instance.
+    ///
+    /// - Parameters:
+    ///   - name: Name of the item
+    ///   - health: Health of the item. Defaults
+    ///             to 100. Ranges from 0 - 100.
+    ///             Snaps to the nearest valid value
+    ///             if not in range.
     init(_ name: String, health: Int = 100) {
         self.name = name
         self.health = health
     }
     
+    /// Increases the health of the item.
+    ///
+    /// - Parameter amount: Amount by which to increase
+    ///                     the health of the item.
+    ///                     Can not over repair. Health
+    ///                     value snaps to the nearest
+    ///                     valid value if not between
+    ///                     0 - 100.
     func repair(by amount: Int) {
         health += amount
     }
     
-    func hurt(by amount: Int = 10) {
+    /// Decreases the health of the item.
+    ///
+    /// - Parameter amount: Amount by which to decrease
+    ///                     the health of the item.
+    ///                     Can not over weaken. Health
+    ///                     value snaps to the nearest
+    ///                     valid value if not between
+    ///                     0 - 100.
+    func weaken(by amount: Int) {
         health -= amount
     }
 }
